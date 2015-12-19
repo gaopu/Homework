fb2fe2563303b5f3703e8baba605dadadd775455
gaopu <936302529@qq.com>
2015-12-19 19:48:12 +0800

    修改最小重量机器设计代码

--- 算法/1.cpp
+++ 算法/1.cpp
@@ -5,9 +5,9 @@
 **/
 #include <iostream>
 #include <cmath>
-
+ 
 using namespace std;
-
+ 
 const int width = 3;
 const int height = 3;
 //列表示厂商
@@ -16,21 +16,21 @@ const int height = 3;
 //结果是:最少钱数4,最小重量4
 int weight[height][width] = {1,2,3,3,2,1,2,2,2};
 int money[height][width] = {1,2,3,3,2,1,2,2,2};
-
+ 
 //部件个数
-int componentCount = 0;
+int componentCount = 3;
 //厂商个数
-int companyCount = 0;
+int companyCount = 3;
 //最高价格
-int maxMoney = 0;
-
+int maxMoney = 4;
+ 
 //表示当前整个机器的重量
 int nowWeight = 0;
 //表示当前整个机器的价格
 int nowMoney = 0;
 //表示每个部件的厂商id
 int companyIdOfComponent[height] = {0};
-
+ 
 //参数表示部件id
 void calc(int componentId) {
     //对当前部件遍历每一个厂商
@@ -41,38 +41,48 @@ void calc(int componentId) {
                 //更改重量
                 nowWeight -= weight[componentId][companyIdOfComponent[componentId]];
                 nowWeight += weight[componentId][i];
-
+ 
                 //更改钱数
                 nowMoney -= money[componentId][companyIdOfComponent[componentId]];
                 nowMoney += money[componentId][i];
-
+ 
                 //更改此部件选择的厂商id
                 companyIdOfComponent[componentId] = i;
             }
         }
-        //当前钱数没有超过最高限制,那么必须要比现在的钱数小,才替换
+        //当前钱数没有超过最高限制,那么必须要比现在的重量小,并且替换后钱数不超过最大钱数,才替换为现在的这个厂商
         else {
-            if (money[componentId][i] < money[componentId][companyIdOfComponent[componentId]]) {
-                //更改重量
-                nowWeight -= weight[componentId][companyIdOfComponent[componentId]];
-                nowWeight += weight[componentId][i];
-
-                //更改钱数
-                nowMoney -= money[componentId][companyIdOfComponent[componentId]];
-                nowMoney += money[componentId][i];
-
-                //更改此部件选择的厂商id
-                companyIdOfComponent[componentId] = i;
+            //重量比当前小
+            if (weight[componentId][i] < weight[componentId][companyIdOfComponent[componentId]]) {
+                 
+                //减去本部件原先花的钱数后其他部件花的钱数
+                int lastMoney = nowMoney - money[componentId][companyIdOfComponent[componentId]];
+                //更改为当前厂商后花的总钱数
+                int nextMoney = lastMoney + money[componentId][i];
+ 
+                //钱数不超过最大钱数
+                if (nextMoney < maxMoney) {
+                    //更改重量
+                    nowWeight -= weight[componentId][companyIdOfComponent[componentId]];
+                    nowWeight += weight[componentId][i];
+ 
+                    //更改钱数
+                    nowMoney -= money[componentId][companyIdOfComponent[componentId]];
+                    nowMoney += money[componentId][i];
+ 
+                    //更改此部件选择的厂商id
+                    companyIdOfComponent[componentId] = i;
+                }
             }
         }
-
+ 
         //如果还有部件没有计算到,继续下去
         if (componentId < componentCount - 1) {
             calc(componentId + 1);
         }
     }
 }
-
+ 
 //使最开始的状态是:每个零件选择0号厂商
 void init() {
     for (int i = 0;i < componentCount;i++) {
@@ -81,11 +91,11 @@ void init() {
         nowWeight += weight[i][0];
     }
 }
-
+ 
 int main(int argc, char const *argv[]) {
-    cout<<"请依次输入部件个数,厂商个数,钱数上限:";
+    /*cout<<"请依次输入部件个数,厂商个数,钱数上限:";
     cin>>componentCount>>companyCount>>maxMoney;
-
+ 
     cout<<"请依次输入重量信息(横向输入,1行代表1个部件在所有厂商处的重量,下同):";
     for (int i = 0;i < componentCount;i++) {
         for (int j = 0;j < companyCount;j++) {
@@ -93,17 +103,17 @@ int main(int argc, char const *argv[]) {
         }
     }
     cout<<"请依次输入价格信息(横向输入):";
-    for (int i = 0;i < componentCount;i++) {
+    for (i = 0;i < componentCount;i++) {
         for (int j = 0;j < companyCount;j++) {
             cin>>money[i][j];
         }
-    }
-
+    }*/
+ 
     //初始化
     init();
     //计算,从0号部件开始
     calc(0);
-
+ 
     if (nowMoney > maxMoney) {
         cout<<"没有符合要求的选择."<<endl;
     } else {
@@ -112,9 +122,9 @@ int main(int argc, char const *argv[]) {
             cout<<companyIdOfComponent[i]<<" ";
         }
         cout<<endl;
-
+ 
         cout<<"总钱数:"<<nowMoney<<" "<<"总重量:"<<nowWeight<<endl;
     }
-
+ 
     return 0;
 }
